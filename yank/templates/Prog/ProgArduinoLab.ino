b0323073054fb70cd91995b66aee02547316ccbc
Ryan Magdaleno <ryamagdaleno@gmail.com>
2024-02-24 22:58:00 -0600

    : : Yank

---
+++ yank/templates/Prog/ProgArduinoLab.ino
@@ -0,0 +1,39 @@
+/* rmagd2Lab#.ino */
+/* ---------------------------------------------------------------------------------------
+ >> Assignment details and provided code are created and owned by Patrick Troy.
+ >> University of Illinois at Chicago - CS 362, Spring 2024
+ >> --------------------------------------------------------------------------------------
+ >> File    :: rmagd2Lab2.ino :: Lab # - NAME
+ >> Course  :: CS 362 (47019), SP24
+ >> Author  :: Ryan Magdaleno
+ >> UIN/nID :: 668523658 (rmagd2)
+ >> System  :: Windows 10 w/ Mingw-w64
+ >> TA      :: ...
+ - -                               - -
+ >> References used ::
+ >> ...
+ - -                               - -
+ >> File overview ::
+ >> ...
+--------------------------------------------------------------------------------------- */
+// Preprocessor Directives ::
+// Global Variables ::
+const byte pins[] = { };
+
+
+/** setup :: - -                                                                       - -
+Set correct digital pins to OUTPUT/INPUT mode.
+    - -                                                                                 */
+void setup()
+{
+    
+}
+
+
+/** loop :: - -                                                                        - -
+Main Arduino program loop.
+    - -                                                                                 */
+void loop()
+{
+    
+}   //                                                                                  */
